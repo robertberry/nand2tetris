a71baee83c5f3fce4c68f6db75978d8f43d86c62
Robert Berry <rjberry@gmail.com>
2023-09-18 08:22:48 +0900

    Add directory for Chapter 7 VM translator project

---
+++ translator/translator.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    std::cerr << "Usage: translator <file>" << std::endl;
+    return 1;
+  }
+
+  std::cout << "TODO" << std::endl;
+}
