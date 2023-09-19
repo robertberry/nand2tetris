7e009036a2b2e4bc087e07ffcab1d6874ea0fc66
Robert Berry <rjberry@gmail.com>
2023-09-19 20:36:18 +0900

    Add more tests

--- translator/parser_test.cc
+++ translator/parser_test.cc
@@ -31,5 +31,41 @@ push constant 2145
   EXPECT_TRUE(p.HasMoreLines());
 }
 
+TEST(ParserTest, AdvanceMovesToFirstInstruction) {
+  std::istringstream input(R"pcode(
+push constant 2145
+)pcode");
+  Parser p(input);
+
+  p.Advance();
+
+  EXPECT_EQ(p.CurrentInstruction().command_type, CommandType::kCPush);
+}
+
+TEST(ParserTest, AdvanceInvokedTwiceMovesToSecondInstruction) {
+  std::istringstream input(R"pcode(
+push constant 256
+pop local 32
+)pcode");
+  Parser p(input);
+
+  p.Advance();
+  p.Advance();
+
+  EXPECT_EQ(p.CurrentInstruction().command_type, CommandType::kCPop);
+}
+
+TEST(ParserTest, Arg1ArithmeticIsCommandName) {
+  std::string command = "sub";
+  std::istringstream input(command);
+  Parser p(input);
+
+  p.Advance();
+
+  Instruction instruction = p.CurrentInstruction();
+  EXPECT_EQ(instruction.command_type, CommandType::kCArithmetic);
+  EXPECT_EQ(instruction.arg1, command);
+}
+
 }  // namespace
 }  // namespace translator
