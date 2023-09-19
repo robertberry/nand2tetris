33bc7f98c7153e722a83e8329003477f67d35f98
Robert Berry <rjberry@gmail.com>
2023-09-19 18:39:56 +0900

    Add more test cases

--- translator/parser_test.cc
+++ translator/parser_test.cc
@@ -13,5 +13,23 @@ TEST(ParserTest, HasMoreLinesEmptyStringIsFalse) {
   EXPECT_FALSE(p.HasMoreLines());
 }
 
+TEST(ParserTest, HasMoreLinesOnlyCommentIsFalse) {
+  std::istringstream input(R"pcode(
+// This is a comment
+)pcode");
+  Parser p(input);
+
+  EXPECT_FALSE(p.HasMoreLines());
+}
+
+TEST(ParserTest, HasMoreLinesNonEmptyStringIsTrue) {
+  std::istringstream input(R"pcode(
+push constant 2145
+)pcode");
+  Parser p(input);
+
+  EXPECT_TRUE(p.HasMoreLines());
+}
+
 }  // namespace
 }  // namespace translator
