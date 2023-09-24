731649c00710db59e6971fc23e99e8310d154cec
Robert Berry <rjberry@gmail.com>
2023-09-24 17:08:46 -0400

    Add stub for CompilationEngine

---
+++ compiler/compilation_engine.cc
@@ -0,0 +1,66 @@
+#include "compiler/compilation_engine.h"
+
+namespace jack {
+
+void CompilationEngine::CompileClass() {
+  // TODO
+}
+
+void CompilationEngine::CompileClassVarDec() {
+  // TODO
+}
+
+void CompilationEngine::CompileSubroutine() {
+  // TODO
+}
+
+void CompilationEngine::CompileParameterList() {
+  // TODO
+}
+
+void CompilationEngine::CompileSubroutineBody() {
+  // TODO
+}
+
+void CompilationEngine::CompileVarDec() {
+  // TODO
+}
+
+void CompilationEngine::CompileStatements() {
+  // TODO
+}
+
+void CompilationEngine::CompileLet() {
+  // TODO
+}
+
+void CompilationEngine::CompileIf() {
+  // TODO
+}
+
+void CompilationEngine::CompileWhile() {
+  // TODO
+}
+
+void CompilationEngine::CompileDo() {
+  // TODO
+}
+
+void CompilationEngine::CompileReturn() {
+  // TODO
+}
+
+void CompilationEngine::CompileExpression() {
+  // TODO
+}
+
+void CompilationEngine::CompileTerm() {
+  // TODO
+}
+
+int CompilationEngine::CompileExpressionList() {
+  // TODO
+  return 0;
+}
+
+}  // namespace jack
---
+++ compiler/compilation_engine.h
@@ -0,0 +1,53 @@
+#ifndef COMPILER_COMPILATION_ENGINE_H_
+#define COMPILER_COMPILATION_ENGINE_H_
+
+#include <iostream>
+
+#include "compiler/jack_tokenizer.h"
+
+namespace jack {
+
+class CompilationEngine final {
+ public:
+  CompilationEngine(JackTokenizer& tokenizer, std::ostream& output) : 
+      tokenizer_(tokenizer), output_(output) {}
+
+  void CompileClass();
+
+  void CompileClassVarDec();
+
+  void CompileSubroutine();
+
+  void CompileParameterList();
+
+  void CompileSubroutineBody();
+
+  void CompileVarDec();
+
+  void CompileStatements();
+
+  void CompileLet();
+
+  void CompileIf();
+
+  void CompileWhile();
+
+  void CompileDo();
+
+  void CompileReturn();
+
+  void CompileExpression();
+
+  void CompileTerm();
+
+  int CompileExpressionList();
+
+ private:
+  JackTokenizer& tokenizer_;
+
+  std::ostream& output_;
+};
+
+}  // namespace jack
+
+#endif  // COMPILER_COMPILATION_ENGINE_H_
