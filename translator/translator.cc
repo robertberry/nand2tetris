d0d3b9be1b123b37f740a8156d2a83aa58f3e64b
Robert Berry <rjberry@gmail.com>
2023-09-23 21:44:13 -0400

    Update translator to handle directories

--- translator/translator.cc
+++ translator/translator.cc
@@ -13,31 +13,15 @@ using ::translator::Parser;
 
 constexpr std::string_view kHackAssemblyExtension = "asm";
 
-int main(int argc, char* argv[]) {
-  if (argc != 2) {
-    std::cerr << "Usage: translator <file>" << std::endl;
-    return 1;
-  }
-
-  std::filesystem::path absolute_path = std::filesystem::absolute(argv[1]);
-  std::ifstream input_stream(absolute_path.string());
+void HandleFile(CodeWriter& code_writer, std::filesystem::path file_path) {
+  std::ifstream input_stream(file_path.string());
   if (!input_stream.is_open()) {
-    std::cerr << "Could not open '" << absolute_path << "'" << std::endl;
-    return 2;
+    std::cerr << "Could not open '" << file_path << "'" << std::endl;
+    exit(2);
   }
   Parser parser(input_stream);
 
-  absolute_path.replace_extension(kHackAssemblyExtension);
-  std::ofstream output_stream(absolute_path.string());
-  if (!output_stream.is_open()) {
-    std::cerr << "Could not open '" << absolute_path << "' for writing" 
-              << std::endl;
-    return 2;
-  }
-  CodeWriter code_writer(output_stream);
-  code_writer.SetFileName(absolute_path.filename().string());
-
-  code_writer.WriteBootstrap();
+  code_writer.SetFileName(file_path.filename().string());
 
   while (parser.HasMoreLines()) {
     parser.Advance();
@@ -71,10 +55,60 @@ int main(int argc, char* argv[]) {
 
       default:
         std::cerr << "Unexpected instruction type" << std::endl;
-        return 3;
+        exit(3);
     }
   }
+}
 
+std::filesystem::path GetOutputPath(std::filesystem::path input_path) {
+  if (!std::filesystem::is_directory(input_path)) {
+    input_path.replace_extension(kHackAssemblyExtension);
+    return input_path;
+  }
+
+  // Use same name for file as directory.
+  std::filesystem::path file_name = input_path.filename();
+  file_name.replace_extension(kHackAssemblyExtension);
+  return input_path / file_name;
+}
+
+bool HasVmExtension(const std::filesystem::path& path) {
+  return path.extension() == ".vm";
+}
+
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    std::cerr << "Usage: translator <file>" << std::endl;
+    return 1;
+  }
+
+  std::filesystem::path absolute_path = std::filesystem::absolute(argv[1]);
+  std::filesystem::path output_path = GetOutputPath(absolute_path);
+  std::ofstream output_stream(output_path.string());
+  CodeWriter code_writer(output_stream);
+  
+  if (!output_stream.is_open()) {
+    std::cerr << "Could not open '" << output_path << "' for writing" 
+              << std::endl;
+    return 2;  
+  }
+
+  code_writer.WriteBootstrap();
+  if (std::filesystem::is_directory(absolute_path)) {
+    bool saw_at_least_one_vm_file = false;
+    for (const auto& entry : std::filesystem::directory_iterator(absolute_path)) {
+      if (entry.is_regular_file() && HasVmExtension(entry.path())) {
+        saw_at_least_one_vm_file = true;
+        HandleFile(code_writer, entry.path());
+      }
+    }
+    if (!saw_at_least_one_vm_file) {
+      std::cerr << "No .vm files in '" << absolute_path << "'" << std::endl;
+      exit(4);
+    }
+  } else {
+    HandleFile(code_writer, absolute_path);
+  }
   code_writer.Close();
 
   return 0;
